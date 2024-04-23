0674c4bb981420fb092b52199dd9cb6dcd4f2245
Dev Patel <dpatel222430@gmail.com>
2024-04-24 00:42:08 +0530

    Done Basic UI and Basic Logic

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,17 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var displayText = "0"
+    @State private var currentInput = ""
+    @State private var storedValue :Double? = 0
+    @State private var currentOperator:String?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            
+            // Text View
+            Text(displayText)
+                .font(.system(size: 60))
+            
+            CalcButtons(buttons:["1","2","3","4"])
+            CalcButtons(buttons:["1","2","3","4"])
+            CalcButtons(buttons:["1","2","3","4"])
+            CalcButtons(buttons:["1","2","3","4"])
         }
-        .padding()
+    }
+        
+}
+struct CalcButtons : View {
+    
+    let buttons:[String]
+    //let action:(String)->Void
+    let buttonSize = (UIScreen.main.bounds.width - 5 * 12) / 4
+    
+    var body: some View{
+        
+        HStack(spacing: 12){
+            ForEach(buttons, id:\.self){buttonLabel in
+                Button{
+                    // action: (buttonLabel),
+                }
+                    label: {
+                    Text(buttonLabel)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .frame(width:buttonSize,height: buttonSize)
+                            .background(.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(50.0)
+                }}
+        }
+        
     }
 }
+
 
 #Preview {
     ContentView()
