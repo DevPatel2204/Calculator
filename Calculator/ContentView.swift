828900812f9c4f29225386d59f55f7ff58cc3410
Dev Patel <dpatel222430@gmail.com>
2024-04-26 00:21:07 +0530

    Added the logic and changed some UI Completeed app

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -14,24 +14,75 @@ struct ContentView: View {
     @State private var storedValue :Double? = 0
     @State private var currentOperator:String?
     var body: some View {
-        VStack{
-            
-            // Text View
-            Text(displayText)
-                .font(.system(size: 60))
-            
-            CalcButtons(buttons:["1","2","3","4"])
-            CalcButtons(buttons:["1","2","3","4"])
-            CalcButtons(buttons:["1","2","3","4"])
-            CalcButtons(buttons:["1","2","3","4"])
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                // Text View
+                Text(displayText)
+                    .font(.system(size: 80))
+                    .foregroundColor(.white)
+                
+                CalcButtons(buttons:["7","8","9","/"], action: buttonTap(_:))
+                CalcButtons(buttons:["4","5","6","*"], action: buttonTap(_:))
+                CalcButtons(buttons:["1","2","3","-"], action: buttonTap(_:))
+                CalcButtons(buttons:["0",".","=","+"], action: buttonTap(_:))
+                CalcButtons(buttons:["C"], action: {button in clearbutton()})
+                    
+            }
         }
+    }
+    
+    func buttonTap(_ button:String) {
+        switch button {
+        case "+","-","*","/":
+            currentOperator = button
+            storedValue = Double(currentInput)
+            currentInput = ""
+        case "=":
+            if let operatorType = currentOperator,
+               let storedValue = storedValue,
+               let currentValue = Double(currentInput){
+                let result:Double
+                
+                switch operatorType{
+                case "+":
+                    result = storedValue + currentValue
+                case "-":
+                    result = storedValue - currentValue
+                case "*":
+                    result = storedValue * currentValue
+                case "/":
+                    result = storedValue / currentValue
+                default:
+                    fatalError("Unknown Operator")
+                }
+                currentInput = "\(result)"
+                currentOperator = nil
+            }
+        case ".":
+            currentInput += button
+        default:
+            currentInput += button
+        }
+        if currentInput.isEmpty
+        {
+            displayText = "0"
+        }else{
+            displayText = currentInput
+        }
+    }
+    func clearbutton(){
+        currentInput = ""
+        storedValue = nil
+        currentOperator = nil
+        displayText = "0"
     }
         
 }
 struct CalcButtons : View {
     
     let buttons:[String]
-    //let action:(String)->Void
+    let action:(String)->Void
     let buttonSize = (UIScreen.main.bounds.width - 5 * 12) / 4
     
     var body: some View{
@@ -39,15 +90,16 @@ struct CalcButtons : View {
         HStack(spacing: 12){
             ForEach(buttons, id:\.self){buttonLabel in
                 Button{
-                    // action: (buttonLabel),
+                    action(buttonLabel)
                 }
                     label: {
                     Text(buttonLabel)
                             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                             .frame(width:buttonSize,height: buttonSize)
-                            .background(.gray)
+                            .background(.orange)
                             .foregroundColor(.white)
                             .cornerRadius(50.0)
+                            
                 }}
         }
         
